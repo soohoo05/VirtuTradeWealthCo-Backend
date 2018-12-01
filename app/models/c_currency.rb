f5a97ba28b0d3314daa7ce29acc645b1014d276e
github username <github email address>
2018-12-01 16:01:17 -0500

    test

--- app/models/c_currency.rb
+++
@@ -1,4 +0,0 @@
-class CCurrency < ApplicationRecord
-  has_many :usercryptos
-  has_many :users, through :usercryptos
-end
--- app/models/user_crypto.rb
+++
@@ -1,4 +0,0 @@
-class UserCrypto < ApplicationRecord
-  belongs_to :user
-  belongs_to :ccurrency
-end
