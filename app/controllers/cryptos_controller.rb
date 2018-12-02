5d402a17de8758700aed53fcac3cf4a85f4e1590
github username <github email address>
2018-12-02 10:56:37 -0500

    modify crypros controller

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -3,8 +3,10 @@ class CryptosController < ApplicationController
   def index
     #require 'pry'; binding.pry
     response = RestClient.get("https://api.coinmarketcap.com/v2/ticker")
-    json_response = JSON.parse(response.body)
-    render json: {"currencies": json_response}
+    json_response = JSON.parse(response)
+    #arr = [];
+    #json_response['data'].each { |k,v| arr.push(v)}
+    render json: json_response['data'].map { |k,v| v}
   end
 
   def show
@@ -12,8 +14,8 @@ class CryptosController < ApplicationController
     #require 'pry'; binding.pry
     currency_id = params['id']
     response = RestClient.get("https://api.coinmarketcap.com/v2/ticker/#{currency_id}")
-    json_response = JSON.parse(response.body)
-    render json: {"currencies": json_response}
+    json_response = JSON.parse(response)
+    render json: json_response['data']
   end
 
   def create
---
+++ gpa.sh
@@ -0,0 +1,1 @@
+git push partner && git push nas
